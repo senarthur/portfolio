6477fd9ae7ca4cb762ae57649ffcd4ed1bd0dac8
Arthur Sena <sena.arthur698@gmail.com>
2024-07-09 09:59:42 -0300

    Animations - Home section & Skills section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: '/home',
+        redirectTo: 'home',
         pathMatch: 'full'
     },
     {
