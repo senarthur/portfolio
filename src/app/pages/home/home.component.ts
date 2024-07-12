3647320e176a954ce7e594550b2085e0242b1f76
Arthur Sena <sena.arthur698@gmail.com>
2024-07-12 08:15:10 -0300

    Slider

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 
@@ -14,4 +14,21 @@ import { FooterComponent } from '../../components/footer/footer.component';
 })
 export class HomeComponent {
 
+  @ViewChild('arrowLeft') arrowLeft!: ElementRef;
+  @ViewChild('arrowRight') arrowRight!: ElementRef;
+  @ViewChild('container') container!: ElementRef;
+
+  constructor() { }
+  
+  slideToRight() {
+    this.container.nativeElement.style.left = '-1360px';
+    this.arrowRight.nativeElement.style.visibility = 'hidden';
+    this.arrowLeft.nativeElement.style.visibility = 'visible';
+  }
+
+  slideToLeft() {
+    this.container.nativeElement.style.left = '0px';
+    this.arrowLeft.nativeElement.style.visibility = 'hidden';
+    this.arrowRight.nativeElement.style.visibility = 'visible';
+  }
 }
