0179c13bfc30c061fd0b2ec47631601de043f167
Arthur Sena <sena.arthur698@gmail.com>
2024-07-16 11:47:34 -0300

    Layout Changes - Project Section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -14,21 +14,10 @@ import { FooterComponent } from '../../components/footer/footer.component';
 })
 export class HomeComponent {
 
-  @ViewChild('arrowLeft') arrowLeft!: ElementRef;
-  @ViewChild('arrowRight') arrowRight!: ElementRef;
   @ViewChild('container') container!: ElementRef;
 
-  constructor() { }
-  
-  slideToRight() {
-    this.container.nativeElement.style.left = '-1360px';
-    this.arrowRight.nativeElement.style.visibility = 'hidden';
-    this.arrowLeft.nativeElement.style.visibility = 'visible';
+  constructor() {
   }
 
-  slideToLeft() {
-    this.container.nativeElement.style.left = '0px';
-    this.arrowLeft.nativeElement.style.visibility = 'hidden';
-    this.arrowRight.nativeElement.style.visibility = 'visible';
-  }
+
 }
