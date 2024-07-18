b0f7b1c2c5fdd1b0e3720e3b603ac0c467faae98
Arthur Sena <sena.arthur698@gmail.com>
2024-07-18 09:54:43 -0300

    Interactive Form

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,9 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +11,53 @@ import { RouterLink } from '@angular/router';
   imports: [
     NavbarComponent,
     FooterComponent,
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  constructor() {
+export class HomeComponent implements OnInit {
+
+  contactForm!: FormGroup;
+  message: string = 'Enviar';
+  sending: boolean = false;
+  
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      from_name: ['', Validators.required],
+      from_email: ['', Validators.compose([Validators.required, Validators.email])],
+      reply_to: ['', Validators.required],
+      message: ['', Validators.required]
+    })
+  }
+
+  onSubmit(e: Event) {
+    if(!this.contactForm.valid) return;
+
+    this.sending = true;
+
+    emailjs.init('cBOK5Rwu5DWn42Tq1');
+    emailjs.send('service_z23jxcd', 'template_u05y8rq', {
+      from_name: this.contactForm.value.from_name,
+      from_email: this.contactForm.value.from_email,
+      reply_to: this.contactForm.value.reply_to,
+      message: this.contactForm.value.message,
+    }).then(() => {
+        this.message = "Enviado";
+        setTimeout(() => {
+          this.message = "Enviar";
+        },1000);
+    }) .catch(() => {
+        this.message = "Erro";
+        setTimeout(() => {
+          this.message = "Enviar";
+        },1000);
+    }) .finally(() => {
+      this.sending = false;
+      this.contactForm.reset();
+    })
   }
 }
